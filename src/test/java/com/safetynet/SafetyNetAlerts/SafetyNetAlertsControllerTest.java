f195a669d8a6c071dcfa56b11a5d266e9d3895d7
Vanessa Roy <vr.vanessa.roy@gmail.com>
2023-07-27 11:04:39 +0200

    add tests for safetyNetAlertsController with createPerson()

--- src/test/java/com/safetynet/SafetyNetAlerts/SafetyNetAlertsControllerTest.java
+++ src/test/java/com/safetynet/SafetyNetAlerts/SafetyNetAlertsControllerTest.java
@@ -1,17 +1,21 @@
 package com.safetynet.SafetyNetAlerts;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.safetynet.SafetyNetAlerts.model.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.IOException;
 import java.nio.file.Files;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -142,6 +146,22 @@ public class SafetyNetAlertsControllerTest {
                         content().contentType("application/json"),
                         content().json(loadJson("expectedResultGetFamiliesFromStations2&3.json"))
                 );
+    }
+
+    @Test
+    public void testCreatePersonShouldPass() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        Person person = new Person("firstNameTest", "lastNameTest", "addressTest", "cityTest", "zipTest", "phoneTest", "emailTest");
+        String json = mapper.writeValueAsString(person);
+        mockMvc.perform(post("/person").contentType(MediaType.APPLICATION_JSON)
+                        .content(json).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void testCreatePersonWithoutParameterShouldFail() throws Exception {
+        mockMvc.perform(post("/person").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 
 }
